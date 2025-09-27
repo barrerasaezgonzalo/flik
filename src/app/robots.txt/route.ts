be28660559eb136dfc4f951f6898158bab29c04d
Gonza <barrerasaezgonzalo@gmail.com>
2025-09-27 18:06:56 -0300

    update robots

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -3,6 +3,12 @@ export async function GET() {
     `User-agent: *
 Allow: /
 Sitemap: https://flik.cl/sitemap.xml`,
-    { headers: { "Content-Type": "text/plain" } },
+    {
+      headers: {
+        "Content-Type": "text/plain",
+        "Cache-Control": "no-transform",
+        "X-Robots-Tag": "noai, noimageai",
+      },
+    },
   );
 }
