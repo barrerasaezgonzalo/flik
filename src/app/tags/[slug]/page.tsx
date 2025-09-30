21d8d9846712138fc7de7bae0cb0569c4435bcc1
Gonza <barrerasaezgonzalo@gmail.com>
2025-09-30 01:03:20 -0300

    update tags

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -112,7 +112,7 @@ export default async function TagPage({
       </ul>
 
       <div className="bg-gray-100 p-4 my-8 text-center border border-dashed rounded-lg">
-        <Link href="/contacto">
+        <Link href="/contact">
           <Image
             src="/ads/publica.png"
             alt="¿Quieres colabrar o proponer un tema?, escríbenos"
